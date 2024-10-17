f12fd319205c44300a5820884192a7c68dbb195a
Antoine M-P <antoine@lumen5.com>
2024-10-17 12:33:22 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,70 @@
+use git2::Repository;
+use open;
+use toml;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Config {
+    owner: String,
+    repo_name: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // find git project root
+    let repo = Repository::discover(".")?;
+    let repo_path = repo.path().parent().unwrap().to_str().unwrap();
+    let mut owner = String::new();
+    let mut repo_name = String::new();
+
+
+    // Check if config already defines owner and repo_name
+    let config_path = format!("{}/.git/open_pr.toml", repo_path);
+    if std::fs::metadata(&config_path).is_ok() {
+        let config_str = std::fs::read_to_string(&config_path)?;
+        let config: Config = toml::from_str(&config_str)?;
+        owner = config.owner;
+        repo_name = config.repo_name;
+        println!("Owner: {}", owner);
+        println!("Repository Name: {}", repo_name);
+        println!("You can tweak this configuration in .git/open_pr.toml");
+    }
+    else {
+        // prompt owner and repo_name
+        println!("Enter the owner of the repository (The \"org\" in github.com/org/reponame): ");
+        std::io::stdin().read_line(&mut owner).unwrap();
+        owner = owner.trim().to_string();
+        println!("Enter the repository name: (The \"reponame\" in github.com/org/reponame): ");
+        std::io::stdin().read_line(&mut repo_name).unwrap();
+        repo_name = repo_name.trim().to_string();
+        println!("You can tweak this configuration later in .git/open_pr.toml");
+    }
+
+    // Write to .open_pr.toml
+    let config: Config = Config {
+        owner: owner.clone(),
+        repo_name: repo_name.clone(),
+    };
+    let config_str = toml::to_string(&config).unwrap();
+    std::fs::write(format!("{}/.git/open_pr.toml", repo_path), config_str)?;
+
+    let repo = Repository::open(".")?;
+
+    // Get the current branch
+    let head = repo.head()?;
+    let head_ref = head.shorthand().unwrap_or("unknown");
+    let base_branch = "master"; // You can change this to the branch you want to merge into
+
+    // Build the URL for creating a pull request on GitHub
+    let pr_url = format!(
+        "https://github.com/{}/{}/compare/{}...{}?expand=1",
+        owner, repo_name, base_branch, head_ref
+    );
+
+    // Print the URL (optional)
+    println!("Opening PR URL: {}", pr_url);
+
+    // Open the URL in the default web browser
+    open::that(pr_url)?;
+
+    Ok(())
+}
